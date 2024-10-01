8400c1aae08ad1068dec67ddcc7e806bbfaeb459
Salvatore <salvaorepontillo07@gmail.com>
2024-10-01 16:55:55 +0200

    while loop doesn't work bc of the continue statement, marked with a comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pymongo
 from sta_minchia import alles_zeigen
 from sta_minchia import suche_nach_int
+from sta_minchia import suche_nach_name
+from sta_minchia import suche_in_array
 
 verbindung = pymongo.MongoClient("mongodb://localhost:27017/")
 datenbank = verbindung["Spiele"]
@@ -12,8 +14,12 @@ def main():
         eingabe = input("Enter a command:")
         if eingabe.lower() == "alles":
             alles_zeigen(collection)
+        elif eingabe.lower() == "namen suche":
+            suche_nach_name(collection)
         elif eingabe.lower() == "zahl suche":
             suche_nach_int(collection)
+        elif eingabe.lower() == "array suche":
+            suche_in_array(collection)
         elif eingabe.lower() == "quit":
             print("Die Anwendung wird geschlossen")
             break
--- sta_minchia.py
+++ sta_minchia.py
@@ -8,11 +8,28 @@ def alles_zeigen(collection):
 def suche_nach_name(collection):
     eingabe_name = input("Nach was suchen sie ?").lower()
     namen_suche = {"name": {"$regex": eingabe_name}}
-    for dokument in collection.find(namen_suche):
+
+    #Begin of sorting mechanism
+    eingabe_sortierung = input("Sortieren ? (y/n)").lower()
+    if eingabe_sortierung == "y":
+        sortierungs_element = input("Name, Jahr, Downloads, Bewertung, PEGI ?").lower()
+        ascdesc_input = input("Aufsteigend oder Absteigend ?")
+        if ascdesc_input == "aufsteigend":
+            ascdesc = 1
+        elif ascdesc_input == "absteigend":
+            ascdesc = -1
+        else: print("Aufsteigend oder Absteigend ?")
+    elif eingabe_sortierung == "n":
+        sortierungs_element = None
+    else: print("Sortieren ? (y/n)")
+    #End of sorting mechanism
+
+    for dokument in collection.find(namen_suche).sort(sortierungs_element, ascdesc):
         print(dokument)
 
 
 def suche_nach_int(collection):
+    #Begin of search
     eingabe_modus = input("Jahr, Downloads, Bewertung, PEGI ?").lower()
     eingabe_operator = input("Mehr, weniger oder gleich ?").lower()
     if eingabe_operator == "mehr":
@@ -21,8 +38,39 @@ def suche_nach_int(collection):
         operatorDB = "$lte"
     elif eingabe_operator == "gleich":
         operatorDB = "$eq"
-    else: eingabe_operator
+    else: print("Mehr, weniger oder gleich ?")
     eingabe_nummer = int(input("Nach welche Zahl ?"))
+    #End of search
+
+    #Begin of sorting mechanism
+    eingabe_sortierung = input("Sortieren ? (y/n)").lower()
+    if eingabe_sortierung == "y":
+        sortierungs_element = input("Name, Jahr, Downloads, Bewertung, PEGI ?").lower()
+        ascdesc_input = input("Aufsteigend oder Absteigend ?")
+        if ascdesc_input == "aufsteigend":
+            ascdesc = 1
+        elif ascdesc_input == "absteigend":
+            ascdesc = -1
+        else: print("Aufsteigend oder Absteigend ?")
+    elif eingabe_sortierung == "n":
+        sortierungs_element = None
+    else: print("Sortieren ? (y/n)")
+    #End of sorting mechanism
+
+    #Search query
     such_query = {eingabe_modus: {operatorDB: eingabe_nummer}}
-    for dokument in collection.find(such_query):
+    for dokument in collection.find(such_query).sort(sortierungs_element, ascdesc):
+        print(dokument)
+
+
+def suche_in_array(collection):
+    value_list = []
+    array_field = input("In welchen Feld suchen sie ?")
+    while True:
+        elemente = input("Welche Elemente suchen sie ?")
+        if elemente == "enough":
+            continue        #this loop doesn't work bc of this bitch
+        else:
+            value_list.append(elemente)
+    for dokument in collection.find({array_field: {"$in": value_list}}):
         print(dokument)
